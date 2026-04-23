b7cf4ad07dc5d1a22f58521b88ee86393bd6ffdc
Viviane <106716101+profvivi@users.noreply.github.com>
2026-04-23 00:50:03 -0300

    s

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ for(let i=0;i <botoes.length;i++){
 }
 fim aula2*/
 
-//aula 4 (substituir a aula 2)
+/*aula 4 (substituir a aula 2)
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
@@ -34,4 +34,45 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;*/
+
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
