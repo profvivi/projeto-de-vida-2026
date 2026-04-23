25c91ce4202625c1c9ff1169c38671be90dafb18
Viviane <106716101+profvivi@users.noreply.github.com>
2026-04-23 00:18:34 -0300

    s

--- main.js
+++ main.js
@@ -29,3 +29,8 @@ for(let i=0;i <botoes.length;i++){
     textos[i].classList.add("ativo");
   }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1;
