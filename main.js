5d7ef6943a9cf72aeb96561af509375643183300
Viviane <106716101+profvivi@users.noreply.github.com>
2026-04-23 00:04:11 -0300

    s

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//aula 2
+/*aula 2 (atribui a todos botões e remove a classe ativo e ativa o botão que foi clicado)
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
@@ -7,8 +7,25 @@ for(let i=0;i <botoes.length;i++){
     for(let j=0;j<botoes.length;j++){
       botoes[j].classList.remove("ativo"); 
     }
-    
+
     botoes[i].classList.add("ativo");
   }
 }
-//fim aula2
+fim aula2*/
+
+//aula 4
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let i=0;i <botoes.length;i++){
+  botoes[i].onclick = function(){
+
+    for(let j=0;j<botoes.length;j++){
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  }
+}
