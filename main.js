b3d1d757cba3c29199bfc67dd90d896bace8c91b
Viviane <106716101+profvivi@users.noreply.github.com>
2026-04-23 00:22:13 -0300

    s

--- main.js
+++ main.js
@@ -32,5 +32,6 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
+let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
