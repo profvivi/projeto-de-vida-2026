00ca8b9523b911f95e4eea3b6026a8d50b908460
Viviane <106716101+profvivi@users.noreply.github.com>
2026-04-30 07:43:26 -0300

    s

--- main.js
+++ main.js
@@ -161,7 +161,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-08-20T00:00:00");
+const tempoObjetivo1 = new Date("2026-03-20T00:00:00");
 const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2026-06-01T00:00:00");
